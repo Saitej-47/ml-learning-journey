f23c3b2d0caa974ff96086cba4bdc7fbecac58e2
Saitej-47 <saitej.pasupuleti74@gmail.com>
2026-06-19 22:03:50 +0530

    Added list comprehension and filter examples

--- listcomprehension.py
+++ listcomprehension.py
@@ -3,6 +3,8 @@ num=[x for x in range(1,11)]
 odd_cubes=[x**3 for x in num if x%2!=0]
 
 result=list(filter(lambda x: x >50,odd_cubes))
-print(result)
+print(f"Original numbers: {num}")
+print(f"Odd cubes: {odd_cubes}")
+print(f"Cubes greater than 50: {result}")
 
 
